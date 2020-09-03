e34973d1fd64b3d650272d80313915bb24af5dfb
Pamela Wang <pampam133@gmail.com>
2020-09-02 22:57:21 -0400

    search page ui changes

--- fridge-app/src/App.js
+++ fridge-app/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-/*import logo from './logo.svg';
-/*import './App.css';
+import './App.css';
+import logo from './logo.png';
 
 /*function App() {
   return (
@@ -25,7 +25,13 @@ import React from 'react';
 
 function helloworld(){
 return(
-<h>hello world.</h>
+  <div className = "App">
+    <header className = "App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+      <p>fridge-detective</p>
+      <input type="text" name="search" placeholder="Search..."></input>
+    </header>
+  </div>
 );
 }
 
